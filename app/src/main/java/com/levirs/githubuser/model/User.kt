c161d7f55c796dfd5ef9fa8a7b3ae0de3653d5d1
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2020-07-02 18:29:25 +0700

    make user parcelable

--- app/src/main/java/com/levirs/githubuser/model/User.kt
+++ app/src/main/java/com/levirs/githubuser/model/User.kt
@@ -1,9 +1,13 @@
 package com.levirs.githubuser.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class User(
     val id: Int,
     val userName: String,
     val name: String,
     val avatar: Int,
     val company: String
-)
+): Parcelable
